1da7c6b8d60ff49656fb304474679864bccf1e34
Gavin Pereira <68476117+pexeixv@users.noreply.github.com>
2022-01-23 18:29:49 +0530

    11ty

--- js/index.js
+++ js/index.js
@@ -4,8 +4,7 @@ Numbers.listenForScrollAndStartCounter()
 import * as Notices from './home/notices.js'
 Notices.set()
 
-import * as Testimonials from './home/testimonials.js'
-Testimonials.set()
+
 
 import * as Animation from './home/animation.js'
 Animation.add()
